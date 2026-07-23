79dd26afe64b130832a76fff9c064ebeb04a1c33
PedroVini09 <gamerpdro713@gmail.com>
2026-07-23 08:05:46 -0300

    feat: finalizando o css e comecando o c#

---
+++ MariaEmMeuLar/Models/EmailSettings.cs
@@ -0,0 +1,13 @@
+namespace MariaEmMeuLar.Models;
+
+public class EmailSettings
+{
+    public string SmtpServer { get; set; } = string.Empty;
+    public int Port { get; set; } 
+    
+    public string SenderName { get; set; } = string.Empty;
+    public string SenderEmail { get; set; } = string.Empty;
+    public string SenderPassword { get; set; } = string.Empty;
+    
+    public string ReceiverName { get; set; } = string.Empty;
+}
---
+++ MariaEmMeuLar/Models/ViewModels/ContatoMensagemViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MariaEmMeuLar.Models.ViewModels;
+
+public class ContatoMensagemViewModel
+{
+    [Required(ErrorMessage = "nome é obrigatório")]
+    public string Nome { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "e-mail é obrigatório")]
+    [EmailAddress(ErrorMessage = "Digite um e-mail válido.")]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Numero de telefone é obrigatorio")]
+    public string? Telefone { get; set; }
+    
+    [Required(ErrorMessage = "Selecione o assunto")]
+    public string Assunto { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Digite a mensagem principal da conversa")]
+    public string Mensagem { get; set; } = string.Empty;
+}
